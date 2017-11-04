1cb0748e646e7c0f7ffa5583760f40fd65cebd0a
Konstantin Semenov <semenov.k.u@gmail.com>
2017-11-04 20:52:30 +0300

    Доделать addCategories

--- Product.php
+++ Product.php
@@ -24,7 +24,7 @@ class Product
     var $isbn = ''; // '978-5-94759-201-6'
     var $mpn = ''; // ''
     var $location = ''; // ''
-    var $quantity; // 6
+    var $quantity = 1; // 6
     var $stock_status_id = 7; // 7
     var $image; // 'catalog/images/evangelskie-besedy-na-kazhdyj-den-goda-po-cerkovnym-zachalam-main.jpg'
     var $manufacturer_id = 26; // 26
@@ -59,8 +59,10 @@ class Product
     var $descriptions = [];
     // Язык
     var $language_id = 1; // 1 - Русский
-    // Картинки товара.
+    // Картинки товара. Массив названий картинок
     var $images = [];
+    // Префикс путей картинок
+    var $images_prefix = "catalog/images/";
     // Основная иерархия категорий товара, хлебные крошки
     var $categories = [];
     // alias для категории для seopro
@@ -74,7 +76,7 @@ class Product
     // Файл логов
     var $log_file = 'logs/messages.log';
     // Файл логов ошибок
-    var $error_file = 'logs/errors.log';
+    var $error_file = 'logs/error.log';
 
     function prepareDB()
     {
@@ -83,30 +85,20 @@ class Product
             $this->dbh = new PDO("mysql:host=$this->dbHost;dbname=$this->dbName;charser=utf8", $this->dbLogin, $this->dbPassword, array(
                 PDO::ATTR_PERSISTENT => true, // храним соединение, чтобы не пересоздавать его для каждого товара
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false
             ));
-        } catch (PDOException $e) {            
-            $this->log('Подключение к базе данных не удалось: ' . $e->getMessage());
-            die('Подключение к базе данных не удалось: ');
+        } catch (PDOException $ex) {
+            $this->errorLog('Подключение к базе данных не удалось: ' . $ex->getMessage());
+            die('Подключение к базе данных не удалось!');
         }
     }
 
     // Конструктор класса
-    function __construct($dbHost, $dbLogin, $dbPassword, $dbName)
+    function __construct()
     {
         // Подключаемся к базе
         $this->prepareDB();
-
-        // тестируем, что база работает
-        // http://phpfaq.ru/pdo
-        // https://www.w3schools.com/php/php_mysql_prepared_statements.asp
-        echo "Начало теста<br>";
-        $stmt = $this->dbh->query('SELECT * FROM oc_product');
-        foreach ($stmt as $row) {
-            echo $row['model'] . "\n<br>";
-        }
-        echo "Конец теста<br>";
-        exit;
 
         // Задаем основные параметры товара
         $this->prepareParams();
@@ -128,10 +120,51 @@ class Product
     function addProductToDB()
     {
         echo "addProductToDB()";
-        // надо сохранить product_id
 
-        $query = "INSERT INTO oc_product (product_id, model, sku, upc, ean, jan, isbn, mpn, location, quantity, stock_status_id, image, manufacturer_id, shipping, price, points, tax_class_id, date_available, weight, weight_class_id, length, width, height, length_class_id, subtract, minimum, sort_order, status, viewed, date_added, date_modified) VALUES ('$this->product_id', '$this->model', '$this->sku', '$this->upc','$this->ean','$this->jan','$this->isbn','$this->mpn','$this->location','$this->quantity','$this->stock_status_id','$this->image','$this->manufacturer_id','$this->shipping','$this->price','$this->points','$this->tax_class_id','$this->date_available','$this->weight','$this->weight_class_id','$this->length','$this->width','$this->height','$this->length_class_id','$this->subtract','$this->minimum','$this->sort_order','$this->status','$this->viewed',now(),'$this->date_modified')";
-        mysqli_query($query) or die('Не удалось создать товар в запросе Mysql::' . print_r(error_get_last()));
+        $sql = "INSERT INTO oc_product (product_id, model, sku, upc, ean, jan, isbn, mpn, location, quantity, stock_status_id, image, manufacturer_id, shipping, price, points, tax_class_id, date_available, weight, weight_class_id, length, width, height, length_class_id, subtract, minimum, sort_order, status, viewed, date_added, date_modified)";
+        $sql .= " VALUES (:product_id, :model, :sku, :upc, :ean, :jan, :isbn, :mpn, :location, :quantity, :stock_status_id, :image, :manufacturer_id, :shipping, :price, :points, :tax_class_id, :date_available, :weight, :weight_class_id, :length, :width, :height, :length_class_id, :subtract, :minimum, :sort_order, :status, :viewed, now(), :date_modified)";
+
+        $stmt = $this->dbh->prepare($sql);
+        $param = array(
+            'product_id' => $this->product_id,
+            'model' => $this->model,
+            'sku' => $this->sku,
+            'upc' => $this->upc,
+            'ean' => $this->ean,
+            'jan' => $this->jan,
+            'isbn' => $this->isbn,
+            'mpn' => $this->mpn,
+            'location' => $this->location,
+            'quantity' => $this->quantity,
+            'stock_status_id' => $this->stock_status_id,
+            'image' => $this->image,
+            'manufacturer_id' => $this->manufacturer_id,
+            'shipping' => $this->shipping,
+            'price' => $this->price,
+            'points' => $this->points,
+            'tax_class_id' => $this->tax_class_id,
+            'date_available' => $this->date_available,
+            'weight' => $this->weight,
+            'weight_class_id' => $this->weight_class_id,
+            'length' => $this->length,
+            'width' => $this->width,
+            'height' => $this->height,
+            'length_class_id' => $this->length_class_id,
+            'subtract' => $this->subtract,
+            'minimum' => $this->minimum,
+            'sort_order' => $this->sort_order,
+            'status' => $this->status,
+            'viewed' => $this->viewed,
+            'date_modified' => $this->date_modified
+        );
+
+        try {
+            $stmt->execute($param);
+            $this->product_id = $this->dbh->lastInsertId();
+            $this->log("Успешно добавлен ТОВАР №$this->product_id !");
+        } catch (PDOException $ex) {
+            $this->errorLog("Не удалось создать товар. " . $ex->getMessage() . "\t" . $sql . "\t" . $param);
+        }
 
         return $this;
     }
@@ -139,24 +172,84 @@ class Product
     function addDescriptionToDB()
     {
         echo "addDescriptionToDB();<br>";
+
+        $sql = "INSERT INTO oc_product_description (product_id, language_id, name, description, tag, meta_title, meta_h1, meta_description, meta_keyword)";
+        $sql .= " VALUES (:product_id, :language_id, :name, :description, :tag, :meta_title, :meta_h1, :meta_description, :meta_keyword)";
+        $stmt = $this->dbh->prepare($sql);
+        $param = array(
+            'product_id' => $this->product_id,
+            'language_id' => $this->language_id,
+            'name' => $this->descriptions[$this->language_id]['name'],
+            'description' => $this->descriptions[$this->language_id]['description'],
+            'tag' => $this->descriptions[$this->language_id]['tag'],
+            'meta_title' => $this->descriptions[$this->language_id]['meta_title'],
+            'meta_h1' => $this->descriptions[$this->language_id]['meta_h1'],
+            'meta_description' => $this->descriptions[$this->language_id]['meta_description'],
+            'meta_keyword' => $this->descriptions[$this->language_id]['meta_keyword']
+        );
+
+        try {
+            $stmt->execute($param);
+            $this->log("Успешно добавлено ОПИСАНИЕ товара №$this->product_id !");
+        } catch (PDOException $ex) {
+            $this->errorLog("Не удалось создать описание товара №$this->product_id! " . $ex->getMessage() . "\t" . $sql . "\t" . $param);
+        }
+
         return $this;
     }
 
     function addImagesToDB()
     {
         echo "addImagesToDB();<br>";
+
+        try {
+            $sql = "INSERT INTO oc_product_image (product_image_id, product_id, image, sort_order)";
+            $sql .= " VALUES ('', :product_id, :image, '')";
+            $stmt = $this->dbh->prepare($sql);
+
+            $stmt->bindParam(':product_id', $this->product_id);
+            $stmt->bindParam(':image', $image);
+            foreach ($this->images as $t) {
+                $image = $this->images_prefix . $t;
+                $stmt->execute();
+            }
+            $this->log("Успешно добавлены КАРТИНКИ товара №$this->product_id !");
+        } catch (PDOException $ex) {
+            $this->errorLog("Не удалось создать картинки товара №$this->product_id! " . $ex->getMessage());
+        }
+
         return $this;
     }
 
     function addLayoutToDB()
     {
         echo "addLayoutToDB();<br>";
+
+        try {
+            $sql = "INSERT INTO `oc_product_to_layout` (`product_id`, `store_id`, `layout_id`) VALUES (?, 0, 0);";
+            $stmt = $this->dbh->prepare($sql);
+            $stmt->execute(array($this->product_id));
+            $this->log("Успешно добавили LAYOUT к товару №$this->product_id");
+        } catch (Exception $ex) {
+            $this->errorLog("Ошибка добавления LAYOUT. " . $ex->getMessage());
+        }
+
         return $this;
     }
 
     function addMagazineToDB()
     {
         echo "addMagazineToDB();<br>";
+
+        try {
+            $sql = "INSERT INTO `oc_product_to_store` (`product_id`, `store_id`) VALUES (:product_id, 0);";
+            $stmt = $this->dbh->prepare($sql);
+            $stmt->execute(array($this->product_id));
+            $this->log("Успешно назначили MAGAZINE к товару №$this->product_id");
+        } catch (Exception $ex) {
+            $this->errorLog("Ошибка добавления MAGAZINE. " . $ex->getMessage());
+        }
+
         return $this;
     }
 
@@ -164,6 +257,42 @@ class Product
     function addCategoryToDB()
     {
         echo "addCategoryToDB();<br>";
+
+//        6. Добавляем категории, если они не существуют
+//        INSERT INTO `oc_category` (`category_id`, `image`, `parent_id`, `top`, `column`, `sort_order`, `status`, `date_added`, `date_modified`) VALUES
+//        (86, '', 0, 0, 1, 0, 1, '2017-10-23 16:18:37', '2017-10-23 16:18:37'),
+//        (87, '', 86, 0, 1, 0, 1, '2017-10-23 16:18:46', '2017-10-23 16:18:46'),
+//        (88, '', 87, 0, 1, 0, 1, '2017-10-23 16:18:54', '2017-10-23 16:18:54');
+
+        try {
+            foreach ($this->categories as $catName => $v) {
+                // смотрим в базе есть ли такая категория
+                $sql = "SELECT category_id FROM oc_category_description WHERE name LIKE ?";
+                $stmt = $this->dbh->prepare($sql);
+                $stmt->execute(array($v));
+                $catId = $stmt->fetchColumn();
+                if ($catId) {
+                    echo "<h3> CatId='$catId' </h3>";
+                } else {
+                    echo "<h3> Надо создать категорию '$catName' </h3>";
+                }
+            }
+            exit;
+            $sql = "INSERT INTO oc_category (category_id, image, parent_id, top, column, sort_order, status, date_added, date_modified)";
+            $sql .= " VALUES (:category_id, '', :parent_id, 0, 1, 0, 1, now(), now())";
+            $stmt = $this->dbh->prepare($sql);
+
+            $stmt->bindParam(':product_id', $this->product_id);
+            $stmt->bindParam(':image', $image);
+            foreach ($this->images as $t) {
+                $image = $this->images_prefix . $t;
+                $stmt->execute();
+            }
+            $this->log("Успешно добавлены КАРТИНКИ товара №$this->product_id !");
+        } catch (PDOException $ex) {
+            $this->errorLog("Не удалось создать картинки товара №$this->product_id! " . $ex->getMessage());
+        }
+
         return $this;
     }
 
@@ -260,7 +389,6 @@ class Product
         if (isset($_POST[weight_class_id])) {
             $this->weight_class_id = $_POST[weight_class_id];
         }
-
         if (isset($_POST[width])) {
             $this->width = $_POST[width];
         }
@@ -301,6 +429,33 @@ class Product
             "meta_description" => '',
             "meta_keyword" => ''
         );
+        // Картинки
+        if (isset($_POST[images])) {
+            // images = 'img1|img2|img3'
+            try {
+                $this->images = explode('|', $_POST[images]);
+            } catch (Exception $ex) {
+                $this->errorLog("В товаре №$this->product_id КАРТИНКИ заданы с ошибками! POST_[images]=$_POST[images]");
+            }
+        }
+        // Категории
+        if (isset($_POST[categories])) {
+            // $_POST[categories] = 'cat1|cat2|cat3'
+            // $this->categories = array (
+            // 'cat1' => 0, // здесь будет id категории
+            // 'cat2' => 0, // здесь будет id категории
+            // 'cat3' => 0 // здесь будет id категории
+            // );
+            try {
+                $tt = explode('|', $_POST[categories]);
+                foreach ($tt as $t) {
+                    $this->categories[$t] = 0;
+                }
+            } catch (Exception $ex) {
+                $this->errorLog("В товаре №$this->product_id КАТЕГОРИИ заданы с ошибками! POST_[categories]=$_POST[categories]");
+            }
+        }
+
         return $this;
     }
 
@@ -308,11 +463,7 @@ class Product
     {
         // пишем лог в файл $this->log_file
         $fp = fopen($this->log_file, "a"); // Открываем файл в режиме записи
-        $test = fwrite($fp, date("Y-m-d H:i:s") . " # " . $_SERVER['REMOTE_ADDR'] . " # $text\r\n");
-        if ($test)
-            echo 'Данные в файл успешно занесены.';
-        else
-            echo 'Ошибка при записи в файл.';
+        fwrite($fp, date("Y-m-d H:i:s") . " # " . $_SERVER['REMOTE_ADDR'] . " # $text\r\n");
         fclose($fp); //Закрытие файла
     }
 
@@ -320,11 +471,7 @@ class Product
     {
         // пишем лог в файл $this->errors_log
         $fp = fopen($this->error_file, "a"); // Открываем файл в режиме записи
-        $test = fwrite($fp, date("Y-m-d H:i:s") . " # " . $_SERVER['REMOTE_ADDR'] . " # $text\r\n");
-        if ($test)
-            echo 'Данные в файл успешно занесены.';
-        else
-            echo 'Ошибка при записи в файл.';
+        fwrite($fp, date("Y-m-d H:i:s") . " # " . $_SERVER['REMOTE_ADDR'] . " # $text\r\n");
         fclose($fp); //Закрытие файла
     }
 
@@ -345,7 +492,8 @@ class Product
     function mainPotok()
     {
         echo "mainPotok();<br>";
-        $this->checkParams()->addProductToDB()->addDescriptionToDB()->addLayoutToDB()->addCategoryToDB()->setCategoriesToDB()->addAttributesGroupToDB()->addAttributesToDB()->setAttributesToDB();
+        //$this->checkParams()->addProductToDB()->addDescriptionToDB()->addImagesToDB()->addLayoutToDB()->addMagazineToDB()->addCategoryToDB()->setCategoriesToDB()->addAttributesGroupToDB()->addAttributesToDB()->setAttributesToDB();
+        $this->checkParams()->addCategoryToDB();
     }
 
     public function __invoke()
